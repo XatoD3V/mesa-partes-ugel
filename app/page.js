0ec4aa4bf04146ad06892a83e897ca06c4ba1bf5
ХАТО DEV - 윌러 <willerinhozap@gmail.com>
2026-08-17 12:20:29 -0500

    5555

--- app/page.js
+++ app/page.js
@@ -94,7 +94,7 @@ export default async function Home() {
                 <p className="text-xs text-tinta-700">Oficinas conectadas</p>
               </div>
               <div>
-                <p className="font-display text-2 font-semibold text-tinta-950">Horarios flexibles</p>
+                <p className="font-display text-2x1 font-semibold text-tinta-950">24/7</p>
                 <p className="text-xs text-tinta-700">Registro de documentos</p>
               </div>
               <div>
